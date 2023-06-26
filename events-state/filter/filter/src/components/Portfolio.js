618ff4b4175515dada04dab8defef763a67a6d24
nick <nick.khachaturian@.gmail.com>
2023-06-26 13:48:41 +0300

    minor fix

---
+++ events-state/filter/filter/src/components/Portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Portfolio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.filter = filter;
+    this.selected = selected;
+  }
+
+  onSelectFilter = (filter) => {
+    
+  }
+}
+
+export default Portfolio;
